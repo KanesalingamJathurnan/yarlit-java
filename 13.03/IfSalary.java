71a660972d240a7ea0622e795245f05f58ec034e
KanesalingamJathurnan <jathurnanjathu54@gmail.com>
2026-03-13 14:33:15 +0530

    .

---
+++ 13.03/IfSalary.java
@@ -0,0 +1,20 @@
+class IfSalary{
+	public static void main(String args[]){
+		int employeesalary=50000;
+		double tax;
+		double netsalary;
+		
+		System.out.println("Basic Salary: "+employeesalary);
+		
+		if(employeesalary>100000){
+			tax=employeesalary*0.03;
+		}else{
+			tax=employeesalary*0.01;
+		}
+		netsalary=employeesalary-tax;
+		System.out.println("tax:"+tax);
+		System.out.println("netsalary:"+netsalary);
+	}	
+}		
+		
+		
